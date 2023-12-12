f611fcb14d212af8f3604118dc00c0a8e37af407
Dimitri VINET <dimitri.pro@dimitrivinet.com>
2023-12-12 16:03:39 +0100

    redis script

---
+++ Assets/Scripts/redis.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NRedisStack;
+using NRedisStack.RedisStackCommands;
+using StackExchange.Redis;
+
+public class redis : MonoBehaviour
+{
+    public string foo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        IDatabase db = redis.GetDatabase();
+        db.StringSet("foo", "bar");
+        foo = db.StringGet("foo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
