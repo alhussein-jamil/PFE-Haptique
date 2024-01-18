eafbc4793136d5e77773f8488eb1ab8c99775237
AurusLaRuse <maudetb.aure@gmail.com>
2024-01-18 13:38:33 +0100

    mvt brush

--- Assets/Scripts/Brush/Brush_Movement.cs
+++ Assets/Scripts/Brush/Brush_Movement.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Brush_Movement : MonoBehaviour
-{
+public class Brush_Movement : MonoBehaviour{
+
     public Transform startPoint; 
+    public Transform First_vibror; 
+    public Transform Last_vibror; 
     public Transform endPoint;
 
     private Vector3 positionInitiale;
@@ -13,8 +15,10 @@ public class Brush_Movement : MonoBehaviour
     public float height_of_curve;
 
     private float timer = 0.0f;
-    private bool movingForward = false;
+
+    private bool First_movement = false;
     private bool curvedMovement = false;
+    private bool Last_movement = false;
 
     void Start()
     {
@@ -25,47 +29,68 @@ public class Brush_Movement : MonoBehaviour
     {
         // if the button on the Unity scrin is clicked then move toward the finish position
 
-        if (movingForward)
-        {
-            timer += Time.deltaTime;
-            
-            float t = timer / duration_of_wave; //calculate the speed of the movement from startPoint to finishPoint based on the total duration of the movement is seconds 
+        
 
-            
-
-            transform.position = Vector3.Lerp(startPoint.position, endPoint.position, t);
-        }
-
-        if (curvedMovement)
-        {
+        
+            if(First_movement){
             timer += Time.deltaTime;
 
             float t = timer / duration_of_wave;
-            float yOffset = height_of_curve *((t - 1) + (t - 1)*(t - 1)) ;
-             transform.position = Vector3.Lerp(startPoint.position, endPoint.position, t) + new Vector3(0, yOffset, 0);
+            float yOffset = height_of_curve - height_of_curve *4*((t-0.5f)*(t-0.5f)) ;
+            transform.position = Vector3.Lerp(startPoint.position, First_vibror.position, t) - new Vector3(0, yOffset, 0);
+
+            if (timer >= duration_of_wave){
+        
+                timer = 0.0f;
+                First_movement=false;
+                curvedMovement=true;}
+            }
+       
+
+            if(curvedMovement){
+                timer += Time.deltaTime;
+                
+                float t = timer / duration_of_wave; //calculate the speed of the movement from startPoint to finishPoint based on the total duration of the movement is seconds 
+
+                transform.position = Vector3.Lerp(First_vibror.position, Last_vibror.position, t);
+
+                if (timer >= duration_of_wave){
+        
+                timer = 0.0f;
+                curvedMovement=false;
+                Last_movement=true;}
+
+
+                }
+
+
+            if(Last_movement){
+                timer += Time.deltaTime;
+                
+                float t = timer / duration_of_wave;
+                float yOffset = height_of_curve - height_of_curve *4*((t-0.5f)*(t-0.5f)) ;
+                transform.position = Vector3.Lerp(Last_vibror.position, endPoint.position, t) - new Vector3(0, yOffset, 0);
+
+            if (timer >= duration_of_wave){
+        
+                timer = 0.0f;
+                Last_movement=false;
+                }
+            }
+
+        
+
+            
         }
 
         //when the brush is in the correct place turned of the movement and reset the timer 
-        if (timer >= duration_of_wave)
-        {
-            timer = 0.0f;
-            movingForward = false;
-            curvedMovement = false;
-            transform.position = positionInitiale;
-            transform.rotation = Quaternion.Euler(0.0f , -90.0f , 0.0f);
-        }
-    }
+        
+    
 
-    // method that is played by the button on the Unity scene. It starts the brush movement
-    public void StartForwardMovement()
-    {
-        movingForward = true;
-    }
-
-    public void StartCurvedMovement()
+    public void StartMovement()
     {
         transform.rotation = Quaternion.Euler(0.0f, 90.0f, -60.0f);
-        curvedMovement = true;
+        First_movement = true;
     }
 
     // the information about the speed of the brush movement is read from the .csv file during the experiment
@@ -75,74 +100,5 @@ public class Brush_Movement : MonoBehaviour
 
     }
 
-}
-
-/* Version 2 - the brush is going from startPoint to finishPoint and back
-
-public class Brush_Movement : MonoBehaviour
-{
-    public Transform startPoint;
-    public Transform endPoint;
-    private bool isWaiting = false;
-    private bool isMoving = false;
-    public float duration_of_wave;
-   
-    private float waitDuration = 3.0f;
-   
-
-    private void Update()
-    {
-        if (isWaiting)
-        {
-            Wait();
-        }
-        else
-        {
-            Move();
-        }
-    }
-
-    private void Move()
-    {
-        if (isMoving)
-        {
-            
-            Vector3 targetPosition = endPoint.position;
-            float distance = Vector3.Distance(startPoint.position, endPoint.position);
-
-            float currentSpeed = distance  / (duration_of_wave );
-
-            float step = currentSpeed * Time.deltaTime ;
-
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
-
-            if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
-            {
-                isWaiting = true;
-                isMoving = false;
-            }
-        }
-    }
-
-    private void Wait()
-    {
-        waitDuration -= Time.deltaTime;
-
-        if (waitDuration <= 0.0f)
-        {
-            waitDuration = 3.0f;
-            isWaiting = false;
-        }
-    }
-
-    public void StartForwardMovement()
-    {
-        transform.position = startPoint.position;
-     
-        isWaiting = false;
-        isMoving = true;
-    }
-
 
 }
-*/
