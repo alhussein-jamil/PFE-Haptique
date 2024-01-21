9e8adce06e8a108fe8692ffe273fb7325b4e10ad
alhussein-jamil <alhussein.jamil@polytechnique.edu>
2024-01-21 06:22:36 +0100

    Add new scripts and scenes

--- Assets/Scripts/Franka/CaresseManager.cs
+++ Assets/Scripts/Franka/CaresseManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using StackExchange.Redis;
 using UnityEngine;
 
 namespace Franka
--- Assets/Scripts/Franka/command.cs
+++ Assets/Scripts/Franka/command.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using StackExchange.Redis;
 using UnityEngine;
+
 namespace Franka
 {
     public class Command : MonoBehaviour
