6d19c0bff28558af12fe58bd6dd68f44213a6071
alhussein-jamil <alhussein.jamil@polytechnique.edu>
2024-01-29 15:11:29 +0100

    Add UDPRedisClient and debugCanvas scripts

---
+++ Assets/UDPRedisClient.cs
@@ -0,0 +1,40 @@
+using Franka;
+using UnityEngine;
+
+public class UDPRedisClient : MonoBehaviour
+{
+    public GameObject gameManager;
+    public RedisConnection redisConnection;
+    public bool subscribed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");   
+        redisConnection = gameManager.GetComponent<RedisConnection>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!subscribed && redisConnection.doneInit)
+        {
+            var x = redisConnection.subscriber.Subscribe(redisConnection.redisChannels["haptic_udp"]);
+
+            x.OnMessage(message => {
+                string msg = message.Message.ToString();
+                if(msg == "start")
+                    UDPManager.Instance.StartCom();
+                else if(msg == "bind")
+                {
+                    UDPManager.Instance.dataReceived += UDPManager.Instance.OnUDPMarginQueueReceived;
+                }
+                UDPManager.Instance.SendData(message.Message);
+            });
+            
+
+            subscribed = true;
+        }
+        
+    }
+}
